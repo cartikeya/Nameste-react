642834f5179a7780de0eef75cb12bbf3dbd494cc
cartikeya <cartikeya.official@gmail.com>
2025-06-04 20:51:52 +0530

    ep-01

---
+++ app.js
@@ -0,0 +1,35 @@
+/*
+div
+    div 
+        h1
+    div
+div
+
+
+*
+*/
+
+const heading  = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement(
+        "div",
+        { id: "child" },
+        [
+            React.createElement("h1",{},"Hi I am h1 tag"),
+            React.createElement("h2",{},"Hi I am h2 tag")
+        ]),
+        React.createElement(
+        "div",
+        { id: "child" },
+        [
+            React.createElement("h1",{},"Hi I am h1 tag"),
+            React.createElement("h2",{},"Hi I am h2 tag")
+        ])
+    ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+        
+root.render(heading)
