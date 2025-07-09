eb71bef8acc69dd5fe298a8ca93ae340ecc32b4a
cartikeya <cartikeya.official@gmail.com>
2025-07-09 14:18:24 +0530

    exp done

--- src/components/ResMenu.js
+++ src/components/ResMenu.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+
 import { useParams } from "react-router";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCatogory from "./RestaurantCategory";
 
 const ResMenu = (props) => {
   // const [showMenu, setshowMenu] = useState(false);
@@ -13,29 +14,22 @@ const ResMenu = (props) => {
   // console.log(resInfo);
   const { name, costForTwoMessage, cuisines } =
     resInfo?.cards[2]?.card?.card?.info;
-  console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR);
+  console.log(resInfo);
+
+  const catogories =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap.REGULAR.cards.filter(
+      (c) =>
+        c?.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  console.log(catogories);
+
   return (
-    <div className="menu">
-      <h2>{name}</h2>
+    <div className="text-center">
+      <h2 className="font-bold my-10 text-4xl">{name}</h2>
       <h1>{costForTwoMessage}</h1>
       <h1>{cuisines.join(", ")}</h1>
-      
-      <h3>
-        {
-          resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
-            ?.card?.title
-        }
-      </h3>
-      <ul>
-        {resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards?.map(
-          (item) => (
-            <li key={item.card.info.id}>
-              {item.card.info.name} - Rs.{" "}
-              {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-            </li>
-          )
-        )}
-      </ul>
+      {catogories.map((catogory) => <RestaurantCatogory catogory={catogory}/>)}
     </div>
   );
 };
---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,5 @@
+const RestaurantCatogory = (props) => {
+  const title =  props?.category?.card?.card?.title;
+  return <h1>{title}</h1>;
+};
+export default RestaurantCatogory;
